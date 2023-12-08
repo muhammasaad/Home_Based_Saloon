2450069fb60f184bddfc23042df0d81fa79e1f02
Saadi-Official927 <124785675+Saadi-Official927@users.noreply.github.com>
2023-12-08 12:11:08 +0500

    User Auth

--- ResponseHandling.js
+++ ResponseHandling.js
@@ -1,15 +1,17 @@
 class ResponseHanding {
-    constructor(res, statusCode, message, success, data) {
+    constructor(res, statusCode, message, success, data, token) {
         this.statusCode = statusCode
         this.message = message
         this.success = success
         this.data = data
         this.res = res
-
+        this.token = token
+        
         return res.status(statusCode).json({
             success:success,
             message:message,
-            data:data
+            data:data,
+            token: token
         })
     }
 }
